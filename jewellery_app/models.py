1daa8e314348652bee664ca004e9fc430d8e3f52
Sagar Singh <sagar966028@gmail.com>
2023-04-07 12:55:24 +0530

    Initial commit

---
+++ jewellery_app/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.utils import timezone
+from django.utils.translation import ugettext_lazy as _
+from PIL import Image
+
+# Create your models here.
+
+class Slider(models.Model):
+    # id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=200, null=True, blank=True)
+    subtitle = models.CharField(max_length=200, null=True, blank=True)
+    # img = models.ImageField(upload_to='slider/', null=True, blank=True)
+    featured = models.ImageField(upload_to='featured/', null=True, blank=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
---
+++ jewellery_app/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render, get_object_or_404, redirect, HttpResponse
+from .models import Slider
+from django.utils import timezone
+
+# Create your views here.
+
+def index(request):
+    # sliders = get_object_or_404(Slider)
+    # sliders = Slider.objects.all()
+    # sliders = get_object_or_404(Slider)
+    sliders = Slider.objects.filter(
+        published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'index.html', {'sliders': sliders})
+    # return render(request, 'index.html')
+    # return HttpResponse("this is homepage")
